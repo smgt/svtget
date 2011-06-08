f7f13b38b621deb33737837f5e35a717890f835e
Sikevux <sikevux@sikevux.se>
2011-06-08 22:51:11 +0200

    Optimizing the Python interface

--- python/Main.py
+++ python/Main.py
@@ -14,8 +14,7 @@ if user_input_url.startswith("http://svt") is not True:
 	print("Bad URL. Not SVT Play?")
 	sys.exit()
 
-final_url = "http://svtget.se/get/get.php?" + str(urllib.parse.urlencode({"url" : user_input_url}).encode('utf-8'), 'utf-8')
-HTTP_socket = urllib.request.urlopen(final_url)
+HTTP_socket = urllib.request.urlopen("http://svtget.se/get/get.php?" + str(urllib.parse.urlencode({"url" : user_input_url}).encode('utf-8'), 'utf-8'))
 HTML_source = HTTP_socket.read().decode('utf-8')
 HTTP_socket.close()
 io = io.StringIO(HTML_source)
@@ -28,10 +27,6 @@ n=0
 for tcUrl in json_data['tcUrls']:
 	print(n, "	" + tcUrl[1] + "	" + tcUrl[0])
 	n+=1
-
-stream = json_data['tcUrls']
-
-lfile = json_data['program_name'] + ".mp4"
 
 user_input_n = int(input("Which file do you want? [#] "))
 
